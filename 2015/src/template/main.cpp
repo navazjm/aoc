58ecad1b5431e0d63c4b012bca39cd2add7f94c5
navazjm <michaelnavs@gmail.com>
2023-11-13 13:00:00 -0600

    2015 template dir - read input file from command line arg

--- 2015/src/template/main.cpp
+++ 2015/src/template/main.cpp
@@ -4,8 +4,13 @@ void partOne(std::string input) { std::cout << input << "\n"; }
 
 void partTwo(std::string input) { std::cout << input << "\n"; }
 
-int main() {
-  std::string filename = "./input.txt";
+int main(int argc, char *argv[]) {
+  // Check if a file path is provided as a command-line argument
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename.txt>" << std::endl;
+    return 1; // Exit with an error code
+  }
+  std::string filename = argv[1];
 
   std::ifstream file(filename);
 
